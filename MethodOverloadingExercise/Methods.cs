96aa8234066b56369bfe86e555c828c706d8d49c
Bradley-Coy <bradleycoy95@gmail.com>
2022-07-12 08:01:16 -0700

    first commit

---
+++ MethodOverloadingExercise/Methods.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace MethodOverloadingExercise
+{
+    internal class Methods
+    {
+        public Methods()
+        {
+        }
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static decimal Add(decimal x, decimal y)
+        {
+            return x + y;
+        }
+
+        public static string Add(int x, int y, bool z)
+        {
+            if (z == true && x + y != 1)
+            {
+                return $"{x + y} dollars";
+            }
+
+            else if (z == true && x + y == 1)
+            {
+                return $"{x + y} dollar"; 
+            }
+            else
+            {
+                return $"{x + y}";
+            }
+        }
+    }
+}
+
---
+++ MethodOverloadingExercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using MethodOverloadingExercise;
+
+//var answer = Methods.Add(2, 2);
+
+
+//var answer = Methods.Add(2232m, 23212m);
+
+//Console.WriteLine(answer);
+
+//var answer = Methods.Add(10, 10, true);
+//Console.WriteLine(answer);
+
+int[] myArray = new int[3];
+myArray[0] = 10;
+myArray[1] = 20;
+myArray[2] = 30;
+
+int[] myArray2 = new int[3] {100, 200, 300};
+
+int[] myArray3 = new int[] {100, 200, 300};
+
+var myArray4 = new int[] { 10_000, 20_000, 30_000 };
+
+int[] myArray5 = { -1, -2, -2 };
+
+foreach (var num in myArray)
+{
+    Console.WriteLine(num);
+
+}
